0022366a9b923f633b24920a37d02941a3cefa66
Alex Hernandez <alexh385@gmail.com>
2016-07-17 21:19:05 -0400

    Add empty markers array to the model
    Add getStyles, getLocations, and getMarkers
    functions to the octopus
    
    Add fillInfowindow function to the octopus
    to put the title and a panorama image inside
    the infowindow when clicked
    
    Add infowindow and bounds variables to
    the viewMap render function
    
    Add click event to viewMap render function
    to open infowindow when marker is clicked
    
    	modified:   js/app2.js

--- js/app2.js
+++ js/app2.js
@@ -2,11 +2,10 @@
 
 // Google maps API
 
-// var map;
-
-var markers = [];
+var map;
 
 var model = {
+	// https://snazzymaps.com/style/70/unsaturated-browns
 	styles: [
 		{
 			"elementType":"geometry",
@@ -189,12 +188,60 @@ var model = {
 	    // 	title: 'Park Ave Penthouse', 
 	    // 	location: {lat: 40.7713024, lng: -73.9632393}
 	    // }		
-	]
+	],
+	markers: []
 };
 
 var octopus = {
 	init: function() {
 		viewMap.init();
+	},
+	getStyles: function() {
+		return model.styles;
+	},
+	getLocations: function() {
+		return model.locations;
+	},
+	getMarkers: function() {
+		return model.markers;
+	},
+	fillInfoWindow: function(marker, infowindow) {
+		// First check to make sure the infowindow is already open
+		if(infowindow.marker != marker) {
+			// Clear the infowindow content to give streetview time to load
+			infowindow.setContent('');
+			infowindow.marker = marker;
+			// infowindow.open(map, marker);
+			// Clear marker property when window is closed
+			infowindow.addListener('closeclick', function() {
+				infowindow.marker = null;
+			});
+			var streetViewService = new google.maps.StreetViewService();
+			var radius = 50;
+
+			// If the pano is found, compute position of the streetview image,
+			// then calculate heading, get the pano and set options
+			function getStreetView(data, status) {
+				if(status == google.maps.StreetViewStatus.OK) {
+					var nearStreetViewLocation = data.location.latLng;
+					var heading = google.maps.geometry.spherical.computeHeading(nearStreetViewLocation, marker.position);
+						infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+						var panoramaOptions = {
+							position: nearStreetViewLocation,
+							pov: {
+								heading: heading,
+								pitch: 0
+							}
+						};
+					var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
+				} else {
+					infowindow.setContent('<div>' + marker.title + '</div>' + '<div>No Street View Found</div>');
+				}
+			}
+			// Use the streetview to get an image witin 50 meters of the marker
+			streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+			infowindow.open(map, marker);
+		}
 	}
 };
 
@@ -203,19 +250,55 @@ var viewMap = {
 		this.render();
 	},
 	render: function() {
-		var map = new google.maps.Map(document.getElementById('map'), {
+		// Render map
+		map = new google.maps.Map(document.getElementById('map'), {
 			center: {lat: 40.750568, lng: -73.99351899999999},
 			zoom: 13,
-			styles: model.styles
+			styles: octopus.getStyles()
 		});
+		// Render markers with infowindow click events
+		var infoWindow = new google.maps.InfoWindow();
+		var bounds = new google.maps.LatLngBounds();
+		var locations = octopus.getLocations();
+		var markers = octopus.getMarkers();
+
+		// Loop through and create markers for each speakeasy object  
+		// using the locations array when the app loads.
+		for(var i = 0; i < locations.length; i++) {
+			
+			var position = locations[i].location;
+			var title = locations[i].title;
+
+			var marker = new google.maps.Marker({
+				map: map,
+				position: position,
+				title: title,
+				animation: google.maps.Animation.DROP,
+				id: i
+			});
+			// Put the new marker into our markers array
+			markers.push(marker);
+			// Give each marker a onclick event to open an info window
+			marker.addListener('click', function() {
+				octopus.fillInfoWindow(this, infoWindow);
+			});
+			bounds.extend(markers[i].position);
+
+			// // Click events for icons during mouseover and mouseout
+			// marker.addListener('mouseover', function() {
+			// 	this.setIcon(highlightIcon);
+			// });
+			// marker.addListener('mouseout', function() {
+			// 	this.setIcon(defaultIcon);
+			// });
+		}
+		// Extend the boundaries of the map for each marker
+		map.fitBounds(bounds);
 	}
 };
-// octopus.init();
 
 // function initMap() {
-// 	// https://snazzymaps.com/style/70/unsaturated-browns
  
-// 	// Tell the API where to create the map
 // 	// Create the first marker
 // 	// var home =	{lat: 40.4458893, lng: -74.52377100000001};
 // 	// var marker = new google.maps.Marker({
@@ -225,90 +308,17 @@ var viewMap = {
 // 	// });
 	
 
-// 	var infoWindow = new google.maps.InfoWindow();
-// 	var bounds = new google.maps.LatLngBounds();
-
 // 	// Style the speakeasy icon
 // 	var defaultIcon = makeMarkerIcon('0091ff');
 
 // 	// Change the color of the icon when the mouse is over it
 // 	var highlightIcon = makeMarkerIcon('FFFF24');
 
-// 	// Loop through and create markers for each speakeasy object using 
-// 	// the locations array when the app loads.
 
-// 	var locationsArray = model.locations;
-
-// 	for(var i = 0; i < locationsArray.length; i++) {
-		
-// 		var position = locationsArray[i].location;
-// 		var title = locationsArray[i].title;
-
-// 		var marker = new google.maps.Marker({
-// 			map: map,
-// 			position: position,
-// 			title: title,
-// 			animation: google.maps.Animation.DROP,
-// 			id: i
-// 		});
-// 		// Put the new marker into our markers array
-// 		markers.push(marker);
-// 		// Give each marker a onclick event to open an info window
-// 		marker.addListener('click', function() {
-// 			fillInfoWindow(this, infoWindow);
-// 		});
-// 		bounds.extend(markers[i].position);
-
-// 		// Click events for icons during mouseover and mouseout
-// 		marker.addListener('mouseover', function() {
-// 			this.setIcon(highlightIcon);
-// 		});
-// 		marker.addListener('mouseout', function() {
-// 			this.setIcon(defaultIcon);
-// 		});
-// 	}
-// 	// Extend the boundaries of the map for each marker
-// 	map.fitBounds(bounds);
+	
 // }
 
-// function fillInfoWindow(marker, infowindow) {
-// 	// First check to make sure the infowindow is already open
-// 	if(infowindow.marker != marker) {
-// 		// Clear the infowindow content to give streetview time to load
-// 		infowindow.setContent('');
-// 		infowindow.marker = marker;
-// 		// infowindow.open(map, marker);
-// 		// Clear marker property when window is closed
-// 		infowindow.addListener('closeclick', function() {
-// 			infowindow.marker = null;
-// 		});
-// 		var streetViewService = new google.maps.StreetViewService();
-// 		var radius = 50;
 
-// 		// If the pano is found, compute position of the streetview image,
-// 		// then calculate heading, get the pano and set options
-// 		function getStreetView(data, status) {
-// 			if(status == google.maps.StreetViewStatus.OK) {
-// 				var nearStreetViewLocation = data.location.latLng;
-// 				var heading = google.maps.geometry.spherical.computeHeading(nearStreetViewLocation, marker.position);
-// 					infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-// 					var panoramaOptions = {
-// 						position: nearStreetViewLocation,
-// 						pov: {
-// 							heading: heading,
-// 							pitch: 0
-// 						}
-// 					};
-// 				var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
-// 			} else {
-// 				infowindow.setContent('<div>' + marker.title + '</div>' + '<div>No Street View Found</div>');
-// 			}
-// 		}
-// 		// Use the streetview to get an image witin 50 meters of the marker
-// 		streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-// 		infowindow.open(map, marker);
-// 	}
-// }
 
 // function makeMarkerIcon(markerColor) {
 // 	var markerImage = new google.maps.MarkerImage('http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
